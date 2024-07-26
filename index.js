8b84bf7e51d1a66f65f55272d81e76912f5f29be
unknown <yohannes.hn16@gmail.com>
2024-07-26 18:18:02 +0700

    Second commit

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-require('dotenv').config()
+require('dotenv').config();
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use(cors()); // Add this line to enable CORS
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-const postsRouter = require('./routes/posts.router')
-const authRouter = require('./routes/auth.router')
+const postsRouter = require('./routes/posts.router');
+const authRouter = require('./routes/auth.router');
 
-app.use("/api/v1/posts", postsRouter)
-app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/posts", postsRouter);
+app.use("/api/v1/auth", authRouter);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log("Server is running....")
-})
+    console.log(`Server is running on port ${PORT}....`);
+});
